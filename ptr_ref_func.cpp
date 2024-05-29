06b701803a62e6f41b7addc75976f9f08e2bc6e4
郭立平 <liping0_0@my.swjtu.edu.cn>
2024-05-29 21:46:48 +0800

    new files created

---
+++ ptr_ref_func.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+
+int func_add(int a, int b)
+{
+    return a + b;
+}
+
+void increase_value(int value)
+{
+    value++;
+}
+
+void increase_pointer(int* value)
+{
+    (*value)++;
+}
+
+void increase_reference(int& value)
+{
+    value++;
+}
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+    int c = func_add(a, b);
+
+    std::cout << "a+b=" << c << std::endl;
+
+    increase_value(a);
+    std::cout << "increase_value(a) = " << a << std::endl;
+
+    increase_pointer(&a);
+    std::cout << "increase_pointer(&a) = " << a << std::endl;
+
+    increase_reference(a);
+    std::cout << "increase_reference(a) = " << a << std::endl;
+
+
+    return 0;
+}
