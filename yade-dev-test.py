c43271863523e367c7034f02fca23276c92bf2b1
郭立平 <liping0_0@my.swjtu.edu.cn>
2024-06-15 12:17:36 +0800

    file renamed

--- yade-dev-test.py
+++ yade-dev-test.py
@@ -1,4 +1,4 @@
-from yade import polyhedra_utils
+from yade import polyhedra_utils, O
 gravel = PolyhedraMat(density = 2600, young = 1E7, poisson = 20000 / 1E7, frictionAngle = 0.5)
 
 list1_polyhedron = [
@@ -24,6 +24,9 @@ list3_polyhedron = [
     [0.0, 0.809, 0.5], [0.0, 0.809, -0.5], [0.0, -0.809, 0.5], [0.0, -0.809, -0.5],
     [0.809, 0.5, 0.0], [0.809, -0.5, 0.0], [-0.809, 0.5, 0.0], [-0.809, -0.5, 0.0]]
     
-polyhedra_utils.fillHull(list3_polyhedron, gravel, sizemin=[0.025, 0.025, 0.025], sizemax=[0.05, 0.05, 0.05], seed=4)
+polyhedra_utils.fillHull(list1_polyhedron, gravel, sizemin=[0.25, 0.25, 0.25], sizemax=[0.5, 0.5, 0.5], seed=1)
+polyhedra_utils.fillHull(list2_polyhedron, gravel, sizemin=[0.025, 0.025, 0.025], sizemax=[0.05, 0.05, 0.05], seed=2)
+polyhedra_utils.fillHull(list3_polyhedron, gravel, sizemin=[0.05, 0.05, 0.05], sizemax=[0.2, 0.2, 0.3], seed=3)
 
+O.bodies.clear()
 
